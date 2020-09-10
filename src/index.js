27761517a7fec477b1bbf373c366d458db33873b
Paul Phukon <hello@snehanshu.tech>
2020-09-10 23:46:50 +0530

    ability to act on received CALLs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ const { nanoid } = require('nanoid');
 const retry = require('async-retry');
 const doWhilst = require('async/doWhilst');
 
+// TODO: Need a more robust version identifier
+/**
+ * Gives the language to use
+ * @param String Version
+ * @returns String
+ */
+const transportLanguage = fp.pipe(
+  _.toString,
+  _.cond([
+    [fp.endsWith('j'), _.constant('JSON')],
+    [fp.endsWith('s'), _.constant('SOAP')],
+    [fp.stubTrue, _.constant(null)],
+  ]),
+);
+
 function formCall(version, action, payload = {}) {
   const language = transportLanguage(version);
   return {
@@ -11,33 +26,66 @@ function formCall(version, action, payload = {}) {
     id: nanoid(10),
   };
 }
+function formCallResult(version, uniqueId, payload = {}) {
+  const language = transportLanguage(version);
+  return {
+    message: 'Not implemented',
+    id: uniqueId,
+  };
+}
+function formCallError(version, uniqueId, errorCode, errorDescription = '', errorDetails = {}) {
+  const language = transportLanguage(version);
+  return {
+    message: 'Not implemented',
+    id: uniqueId,
+  };
+}
 
 function OCPPJParser(message) {
   return {
-    type: 'CALLRESLT',
+    type: 'CALLRESULT',
     id: 'the unique id',
+    action: null,
     payload: {},
   };
 }
 
 function OCPPSParser(message) {
   return {
-    type: 'CALLRESLT',
+    type: 'CALLRESULT',
     id: 'the unique id',
+    action: null,
     payload: {},
   };
 }
 
-function MessageHandler(parser, callManager) {
+function MessageHandler(parser, sentCallsHandler, callHandler, sender, builders) {
   return (message) => {
     const parsed = parser(message);
     switch (parsed.type) {
-      case 'CALL': break;
+      case 'CALL':
+        callHandler(parsed.action, parsed.payload, {
+          callResult(payload) {
+            // Send response using the sender
+            const message2send = _.invoke(builders, 'callResult', parsed.id, payload);
+            if (!_.isUndefined(message2send)) {
+              sender(message2send);
+            }
+          },
+          callError(code, description, details) {
+            // Send response using the sender
+            const message2send = _.invoke(builders, 'callError', parsed.id, code, description, details);
+            if (!_.isUndefined) {
+              sender(message2send);
+            }
+          },
+        });
+        break;
       case 'CALLRESULT':
-        callManager.success(parsed.id, parsed.payload);
+        sentCallsHandler.success(parsed.id, parsed.payload);
         break;
       case 'CALLERROR':
-        callManager.error(parsed.id, parsed.payload);
+        sentCallsHandler.error(parsed.id, parsed.payload);
         break;
       default:
       // ignore
@@ -54,21 +102,6 @@ function extractSender(options, version) {
     ]),
   )(options);
 }
-
-// TODO: Need a more robust version identifier
-/**
- * Gives the language to use
- * @param String Version
- * @returns String
- */
-const transportLanguage = fp.pipe(
-  _.toString,
-  _.cond([
-    [fp.endsWith('j'), _.constant('JSON')],
-    [fp.endsWith('s'), _.constant('SOAP')],
-    [fp.stubTrue, _.constant(null)],
-  ]),
-);
 
 function SentCallsManager(intialStore = {}) {
   const store = _.cloneDeep(intialStore);
@@ -98,21 +131,48 @@ function SentCallsManager(intialStore = {}) {
   };
 }
 
+function ReceivedCallsManager(intialStore = {}) {
+  const store = _.cloneDeep(intialStore);
+
+  function add(action, handler) {
+    store[action] = _.isFunction(handler) ? handler : _.noop;
+  }
+
+  function remove(action) {
+    store[action] = _.noop;
+  }
+
+  function execute(action, ...args) {
+    return _.invoke(store, action, ...args);
+  }
+
+  return { add, remove, execute };
+}
+
+function getBuilders(version) {
+  return {
+    call(action, payload) {
+      return formCall(version, action, payload);
+    },
+    callResult(id, payload) {
+      return formCallResult(version, id, payload);
+    },
+    callError(id, errorCode, errorDescription, errorDetails) {
+      return formCallError(version, id, errorCode, errorDescription, errorDetails);
+    },
+  };
+}
+
 function CSOS(options) {
-  const callManager = SentCallsManager();
+  const sentCallsHandler = SentCallsManager();
+  const receivedCallsHandler = ReceivedCallsManager();
   let currentVersion = null;
   let isConnected = false;
   let sender = _.noop;
+  let msgHandler = _.noop;
+  let builders = {};
+
   const language = () => transportLanguage(currentVersion);
-
-  const msgHandler = (() => {
-    let parser = _.noop;
-    const lang = language();
-    if (lang === 'JSON') parser = OCPPJParser;
-    else if (lang === 'SOAP') parser = OCPPSParser;
-
-    return MessageHandler(parser, callManager);
-  })();
 
   function received(message) {
     if (!isConnected) {
@@ -123,15 +183,32 @@ function CSOS(options) {
   }
 
   function connected(version) {
-    currentVersion = version;
     isConnected = true;
+    currentVersion = version;
     sender = extractSender(options, currentVersion);
+    builders = getBuilders(currentVersion);
+    msgHandler = (() => {
+      let parser = _.noop;
+      const lang = language();
+      if (lang === 'JSON') parser = OCPPJParser;
+      else if (lang === 'SOAP') parser = OCPPSParser;
+
+      return MessageHandler(
+        parser,
+        sentCallsHandler,
+        receivedCallsHandler.execute,
+        sender,
+        builders,
+      );
+    })();
   }
 
   function disconnected() {
-    currentVersion = null;
     isConnected = false;
+    currentVersion = null;
     sender = _.noop;
+    builders = {};
+    msgHandler = _.noop;
   }
 
   /**
@@ -148,17 +225,21 @@ function CSOS(options) {
         return reject(new Error('Not connected to central system yet'));
       }
 
-      const { message, id } = formCall(currentVersion, action, payload);
+      const { message, id } = builders.call(action, payload);
       return retry(() => sender(message))
         .then(() => {
           // Record the id
-          callManager.add(id, resolve, reject);
+          sentCallsHandler.add(id, resolve, reject);
         })
         .catch((error) => {
           // failed permanently
           reject(new Error(`The CALL couldn't be sent: ${_.get(error, 'message', _.toString(error))}`));
         });
     });
+  }
+
+  function onCall(action, handler) {
+    receivedCallsHandler.add(action, handler);
   }
 
   async function bootNotification() {
@@ -188,6 +269,7 @@ function CSOS(options) {
     received,
     sendCall,
     bootNotification,
+    onCall,
     fresh: () => CSOS(options),
   };
 }
