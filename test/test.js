e05f72cfb793d2eedd0c52e0f011b586b8b8cbd5
pSnehanshu <hello@snehanshu.tech>
2020-09-13 11:52:47 +0000

    test written for OCPP JSON parser

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ const { nanoid } = require('nanoid');
 // Ours
 const transportLanguage = require('../src/utils/transportLanguage');
 const Builder = require('../src/builder');
+const OCPPJParser = require('../src/parsers/json');
 
 describe('TransportLanguage utility', function () {
   it('should return null for invalid OCPP version', function () {
@@ -98,4 +99,67 @@ describe('Builder', function () {
       });
     });
   })
+});
+
+describe('Parser', function () {
+  describe('JSON', function () {
+    it('should correctly parse CALL', function () {
+      const uniqueId = nanoid(5);
+      const action = 'Reset';
+      const payload = { type: 'Soft' };
+      const msg = JSON.stringify([2, uniqueId, action, payload]);
+
+      const parsed = OCPPJParser(msg);
+
+      expect(parsed).to.be.an('object');
+      expect(parsed.type).to.equal('CALL');
+      expect(parsed.action).to.equal(action);
+      expect(parsed.id).to.equal(uniqueId);
+      expect(parsed.payload).to.eql(payload);
+    });
+
+    it('should correctly parse CALLRESULT', function () {
+      const uniqueId = nanoid(5);
+      const payload = { status: 'Accepted' };
+      const msg = JSON.stringify([3, uniqueId, payload]);
+
+      const parsed = OCPPJParser(msg);
+
+      expect(parsed).to.be.an('object');
+      expect(parsed.type).to.equal('CALLRESULT');
+      expect(parsed.id).to.equal(uniqueId);
+      expect(parsed.payload).to.eql(payload);
+    });
+
+    it('should correctly parse CALLERROR', function () {
+      const uniqueId = nanoid(5);
+      const errorCode = 'NotImplemented';
+      const errorDescription = 'This feature has not been implemented yet';
+      const errorDetails = {
+        dummy: Math.random() * 10000,
+        dummy2: nanoid(5),
+      };
+      const msg = JSON.stringify([4, uniqueId, errorCode, errorDescription, errorDetails]);
+
+      const parsed = OCPPJParser(msg);
+
+      expect(parsed).to.be.an('object');
+      expect(parsed.type).to.equal('CALLERROR');
+      expect(parsed.id).to.equal(uniqueId);
+      expect(parsed.payload).to.be.an('object');
+      expect(parsed.payload.errorCode).to.equal(errorCode);
+      expect(parsed.payload.errorDescription).to.equal(errorDescription);
+      expect(parsed.payload.errorDetails).to.eql(errorDetails);
+    });
+
+    it('should be able to detect invalid OCPP messges', function () {
+      const invalid1 = '[1, "Hello", {"a":23}]';
+      const invalid2 = 'Not a message';
+      const invalid3 = () => {};
+
+      expect(OCPPJParser(invalid1).type).to.be.null;
+      expect(OCPPJParser(invalid2).type).to.be.null;
+      expect(OCPPJParser(invalid3).type).to.be.null;
+    });
+  });
 });
